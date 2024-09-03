a646d061866d30044747d27894b8d048f3bb8520
= <=>
2024-09-03 18:47:28 +0530

    added own modules

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-var http  = require('http')
+var calc = require('./calc.js')
+result = calc.mul(2,5)
 
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type':'text/html'})
-    res.end("Welcome Back Alien!");
-}).listen(8080)
+result2 = calc.sub(10,5)
+
+console.log("The Output is : "+result);
+console.log("The sub output is "+result2)
